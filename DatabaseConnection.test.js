fe77dfce3f88efbe860abb5242f3eb4ef0ee5e52
surajJha <skj48817@gmail.com>
2018-12-10 19:24:19 +0530

    done with test cases

--- DatabaseConnection.test.js
+++ DatabaseConnection.test.js
@@ -1,22 +1,45 @@
 const DatabaseConnection = require('./DatabaseConnection');
 
+describe('Testing correct query', () => {
+	it('executes query on correct config and query', async () => {
+		const config = {
+			name: 'dev',
+			host: 'localhost',
+			user: 'root',
+			password: '#1234567890',
+			database: 'test',
+			multipleStatements: true,
+			connectionLimit: 10,
+		};
 
-describe('Testing getConnectionPool functon', () => {
-    it('returns a connection pool for correct config', () => {
-        const config = {
-            "name": "dev",
-            "host": "104.211.216.91",
-            "user": "policy_user",
-            "password": "Fre@mW3Rl@123",
-            "database": "los_dev_db1",
-            "multipleStatements": true,
-            "connectionLimit": 100
-        };
-        const db = new DatabaseConnection(config);
-    });
+		const db = DatabaseConnection.getInstance(config);
+		const query = 'select * from users';
+		const result = await db.executeQuery(query, {});
+		expect(result).not.toBe(null);
+		expect(typeof result).toBe('object');
+	});
 
-    it('throws an error on incorrect config', () => {
+	it('throws an error for incorrect config', async () => {
+		const config = {
+			name: 'dev',
+			host: 'localhost',
+			user: '',
+			password: '#1234567',
+			database: 'test',
+			multipleStatements: true,
+			connectionLimit: 10,
+		};
 
-    });
-
+		const db = DatabaseConnection.getInstance(config);
+		db.closePool();
+		db.config = config;
+		db.createConnectionPool();
+		const query = 'select * from users';
+		try {
+			await db.executeQuery(query, {});
+		} catch (e) {
+		 expect(e.message).toEqual("ER_ACCESS_DENIED_ERROR: Access denied for user ''@'localhost' (using password: YES)");
+		}
+	});
 });
+
